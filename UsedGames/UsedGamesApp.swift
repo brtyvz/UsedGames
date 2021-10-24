a396f90ddd21a936cc75fd59ba9eaf29c7b70917
brtyvz <beratttyavuzz@gmail.com>
2021-10-24 21:40:57 +0300

    Initial Commit

---
+++ UsedGames/UsedGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsedGamesApp.swift
+//  UsedGames
+//
+//  Created by Berat Yavuz on 24.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct UsedGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
